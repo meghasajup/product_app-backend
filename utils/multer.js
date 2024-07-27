75a2376005361ff5dcb852937f87ce55a0892464
Megha Saju <meghasaju45@gmail.com>
2024-07-27 21:48:22 +0530

    multer

--- utils/multer.js
+++ utils/multer.js
@@ -1,18 +1,17 @@
-// backend/utils/multer.js
 const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './uploads'); // specify the upload directory
+    cb(null, './uploads'); 
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname); // specify the file name
+    cb(null, Date.now() + '-' + file.originalname); 
   },
 });
 
 const fileFilter = (req, file, cb) => {
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-    cb(null, true); // allow only JPEG and PNG files
+    cb(null, true); 
   } else {
     cb(null, false);
   }
@@ -20,7 +19,7 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 1024 * 1024 * 5 }, // 5MB file size limit
+  limits: { fileSize: 1024 * 1024 * 5 }, 
   fileFilter: fileFilter,
 });
 
